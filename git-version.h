e977cb62fd6ff213af86e1f8b1977cba43d3c645
Deepin Developer <deepin-dev@deepin.org>
2022-09-29 11:24:49 +0800

    feat: Init commit
    init gimp
    
    Log:

---
+++ git-version.h
@@ -0,0 +1,6 @@
+#ifndef __GIT_VERSION_H__
+#define __GIT_VERSION_H__
+#define GIMP_GIT_VERSION "GIMP_2_10_28"
+#define GIMP_GIT_VERSION_ABBREV "9f9ff10"
+#define GIMP_GIT_LAST_COMMIT_YEAR "2021"
+#endif /* __GIT_VERSION_H__ */
